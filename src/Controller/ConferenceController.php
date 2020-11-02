fc662e9156cdf615500b0dea7c9815ed3dc38dea
Tchos Le Milanais <kwenol@yahoo.fr>
2020-11-02 21:59:40 +0100

    Pagination

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
+use Twig\Environment;
 use App\Entity\Conference;
 use App\Repository\CommentRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ConferenceRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Repository\ConferenceRepository;
-use Twig\Environment;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
@@ -24,20 +25,30 @@ class ConferenceController extends AbstractController
         $repos = $conferenceRepository->findAll();
 
         return new Response ($this->twig->render('conference/index.html.twig', [
-            'conferences' => '$repos',
+            'conferences' => $repos,
         ]));
     }
 
     /**
      * @Route("/conference/{id}", name="conference")
      */
-    public function show(Conference $conference, CommentRepository $commentRepository)
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
     {
         $repos = $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']);
+
+        /** 
+         * récupère la valeur du décalage ( offset ) depuis les paramètres de l’URL ( $request->query ) sous forme d’entier ( getInt() ) 
+         * Par défaut, sa valeur sera 0 si le paramètre n’est pas défini
+        */
+        $offset = max(0, $request->query->getInt('offset', 0));
+
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         
         return new Response ($this->twig->render('conference/show.html.twig',[
             'conference' => $conference,
-            'comments' => $repos,
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next'     => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]));
     }
 }
